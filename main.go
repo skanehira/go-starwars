c36a6a8aa17cd4295d40b03d7de35fd855ee0425
skanehira <sho19921005@gmail.com>
2019-01-31 00:30:45 +0900

    improvement bufferd text

--- main.go
+++ main.go
@@ -13,53 +13,59 @@ import (
 )
 
 var (
-	speed  = flag.Int("s", 40, "speed, up speed by making it smaller")
+	// animation speed option
+	speed = flag.Int("s", 40, "speed, up speed by making it smaller")
+	// use colorable to support windows
 	stdOut = bufio.NewWriter(colorable.NewColorableStdout())
+	// screen height
+	height = 14
 )
 
 func clear() {
+	// https://www.grapecity.com/developer/support/powernews/column/clang/047/page02.htm
 	fmt.Fprint(stdOut, "\x1b[2J")
 	fmt.Fprint(stdOut, "\x1b[0;0H")
 	stdOut.Flush()
 }
 
-func parse() {
-	flag.Parse()
-}
-
 func main() {
+	// using packr to embed text in binary
 	file, err := packr.NewBox("./resources").Open("sw1.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	parse()
+	// parse option
+	flag.Parse()
 
-	var buffer []string
+	// use default value
+	var buffer string
 	var duration int
+	var i int
 
-	height := 14
+	// read a line from embed text file
 	scanner := bufio.NewScanner(file)
-
-	i := 0
 	for scanner.Scan() {
 		if i%height == 0 {
+			// get duration from text line
 			duration, err = strconv.Atoi(scanner.Text())
 			if err != nil {
-				panic(err)
+				continue
 			}
 
-			for _, buf := range buffer {
-				fmt.Println(buf)
-			}
+			// print buffered line
+			fmt.Println(buffer)
 
 			time.Sleep(time.Duration((*speed)*duration) * time.Millisecond)
+
+			// clear terminal screen, init buffer and duration
 			clear()
-			buffer = []string{}
+			buffer = ""
 			duration = 0
 		} else {
-			buffer = append(buffer, scanner.Text())
+			// text line into buffer
+			buffer += fmt.Sprintln(scanner.Text())
 		}
 		i++
 	}
